821c26fb09ad309266ffd6c43efeae3c2b6d4708
michel ngako <emoumichel@gmail.com>
2018-09-12 02:46:44 -0400

    array manipulation using for and foreach and reverse

--- UnderstandingArrays1/Program.cs
+++ UnderstandingArrays1/Program.cs
@@ -10,6 +10,9 @@ namespace UnderstandingArrays1
     {
         static void Main(string[] args)
         {
+
+            // wrong way of iterating through a given set fo number
+            /*
             int number1 = 4;
             int number2 = 8;
             int number3 = 15;
@@ -30,6 +33,74 @@ namespace UnderstandingArrays1
 
             }
 
+            */
+
+            /*
+            int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+
+
+            // Console.WriteLine(numbers[1]);
+
+            //determine how many elements are in the array using the lenght property
+
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+
+            */
+
+            // another way of creating an array 
+
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+
+
+            //- one method of iteration using for that will loop through the lenght of 
+            //the array and print out the total numbers of items in the array individualy,
+
+            /*
+            string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee"};
+            /*
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+
+            
+
+            // another method using foreach
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
+            */
+
+            // Reverse array of caracters
+
+            string myResolution = "I will be will a great sofware engeneer like bob tabor as model," +
+                                  " it's just the matter of practice. It's over until I win.";
+
+            char[] charArray = myResolution.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char myResolutionchar in charArray)
+
+            {
+                Console.Write(myResolutionchar);
+            }
+            Console.ReadLine();
+
         }
+
+
     }
+
 }
